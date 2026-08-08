714d58d0f82ab782d9640b3817554b8908f4cd96
NITHYESH SAKTHI <nithyeshs3@gmail.com>
2026-08-08 23:27:03 +0530

    Added the videoID scripts

--- video_stats.py
+++ video_stats.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path = ".env")
 API_KEY = os.getenv("API_KEY")
 CHANNEL = "MrBeast"
+maxResults = 50 #add the max page
 
 def get_playlist_id():
     try:
@@ -26,7 +27,38 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
         #print(f"Error occurred while making the request: {e}")
+ 
+
+
+def get_video_ids(playlistId):
+    video_ids = []
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+                #print(response)
+            data = response.json()
+                #print(json.dumps(data, indent=4))
+            for item in data.get("items",[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            pageToken = data.get('nextPageToken')
+            if not pageToken:
+                break
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 
 if __name__ == "__main__":
+    playlistId = get_playlist_id()
+    print(get_video_ids(playlistId))
     print("API KEY:", API_KEY)
     print("Playlist ID:", get_playlist_id())
