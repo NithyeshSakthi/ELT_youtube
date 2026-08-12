7be15a0c6f46c2fdb7775845197c2b644c7ed04b
NITHYESH SAKTHI <nithyeshs3@gmail.com>
2026-08-13 00:33:33 +0530

    scripts updated to save the ext file &  Added the json data

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 #load the variable paths
 load_dotenv(dotenv_path = ".env")
@@ -99,11 +100,22 @@ def extract_video_data(video_ids):
     
     except requests.exceptions.RequestException as e:
         raise e
+    
+def save_to_json(extracted_data):
+    os.makedirs("./data", exist_ok=True)
+    file_path = f"./data/yt_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii = False)
+
+    print(f"Saved successfully to {file_path}")
 if __name__ == "__main__":  
     #print("API KEY:", API_KEY)
     #print("Playlist ID:", get_playlist_id())
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
     print(extract_video_data(video_ids))
+    extracted_data = extract_video_data(video_ids)
+    save_to_json(extracted_data)
     
 
